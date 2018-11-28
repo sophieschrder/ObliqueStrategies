83a44e50d20a46287209f6d3367b43182b4fd72e
jennybohn <jennybohn@web.de>
2018-11-28 18:46:31 +0100

    Added Karte-annehmen-Button and function for Neue-Karte-Button

--- src/pages/karte-ziehen/karte-ziehen.ts
+++ src/pages/karte-ziehen/karte-ziehen.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Card} from "../../cards";
 import {CardServiceProvider} from "../../providers/card-service/card-service";
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the KarteZiehenPage page.
@@ -20,14 +21,25 @@ export class KarteZiehenPage {
   cards: Array<Card>;
   card: Card;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public myCardsService: CardServiceProvider) {
-    this.cards=  this.myCardsService.getCards()
-    let i= Math.floor(Math.random()*this.cards.length);
-    this.card = this.cards[i];
+  constructor(public navCtrl: NavController, public navParams: NavParams, public myCardsService: CardServiceProvider, public alertCtrl: AlertController) {
+    this.cards=  this.myCardsService.getCards();
+    this.getCard();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad KarteZiehenPage');
   }
-
+  getCard()
+  {
+    let i= Math.floor(Math.random()*this.cards.length);
+    this.card = this.cards[i];
+  }
+  showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'Juuhuuuuu!',
+      subTitle: 'Du hast eine Karte ausgewählt! Viel Spaß beim Spielen!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
