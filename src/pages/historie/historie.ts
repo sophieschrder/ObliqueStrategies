1aa1c997506cadb168600a01515c8c4ba9afcc6e
monique <sawamonique@gmail.com>
2018-12-04 10:26:39 +0100

    historie von ID als carts geladen und dargestellt

--- src/pages/historie/historie.ts
+++ src/pages/historie/historie.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Card} from "../../cards";
 import { Storage } from "@ionic/storage";
+import {CardServiceProvider} from "../../providers/card-service/card-service";
 
 /**
  * Generated class for the HistoriePage page.
@@ -15,18 +17,54 @@ import { Storage } from "@ionic/storage";
   templateUrl: 'historie.html',
 })
 export class HistoriePage {
-  history: number[];
+  // history: number[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
+  /** speichert alle IDs defr Cards in der Historie */
+  ids: Array<object>;
+  
+  /** speichert eine Referenz auf alle Cards */
+  cards: Array<Card>;
+  
+  /** die Cards aus der ID Liste */
+  cardHistory: Array<Card> = new Array<Card>();
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public myCardsService: CardServiceProvider, private storage: Storage) {
+    this.cards = this.myCardsService.getCards();
   }
 
+  /**
+   * wird aufgerufen, wenn diese Seite dargestellt werden soll.
+   */
   ionViewDidLoad() {
+    // laden der Historie als JSON string (wird in data gespeichert)
+    this.storage.get('history').then( ( data ) => {
+      // aus der JSON historie ein ARRAY von OBJECTS machen
+      this.ids = JSON.parse( data );
+      // erzeuge cardHIstory für jede ID in ids
+      for (let i = 0; i < this.ids.length; i++) {
+        // id aus IDs holen
+        const id:number = Number( this.ids[i] );
+        
+        // aus cards die karte mit ID=id suchen
+        for (let c = 0; c < this.cards.length; c++) {
+          const card:Card = this.cards[c];
+          if ( card.id == id )
+          {
+            // karte gefunden, in history kopieren 
+            this.cardHistory.push( card );
+            // aufhören
+            break;
+          }
+        }
+      }
+
+    }); 
     console.log('ionViewDidLoad HistoriePage');
   }
 
   public async showHistory(){
-    this.history= await this.storage.get('history');
-    console.log(this.history);
+    // this.history = await this.storage.get('history');
+    // console.log(this.history);
   }
 
   public clearHistory(){
