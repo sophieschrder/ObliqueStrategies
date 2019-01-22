de4ac157b3a7bf15d0033c4a795a2128fa7347a2
Lena Koester <koesterlen@gmail.com>
2019-01-22 13:11:12 +0100

    Added changes to Profilseite

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {Storage} from "@ionic/storage";
-
 @IonicPage()
 @Component({
   selector: 'page-settings',
@@ -9,18 +8,24 @@ import {Storage} from "@ionic/storage";
 })
 export class SettingsPage {
   history: number[];
-
+  totalCardsPlayed: number;
+  cardHistory: number[];
   constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
   }
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
   }
-
   public async showHistory(){
     this.history= await this.storage.get('history');
+    console.log(this.history);
   }
-
+  public async showBadges(){
+    this.storage.get('history').then((historie) => {
+      this.cardHistory = JSON.parse(historie) || [];
+      this.totalCardsPlayed = this.cardHistory.length;
+      console.log(this.totalCardsPlayed);
+    })
+  }
   public clearHistory(){
     this.storage.clear().then(() => {console.log('Historie entfernt')});
   }
