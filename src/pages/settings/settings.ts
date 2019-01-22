d4d770562376d05aaf509702e96b07b6258908f5
Anzhela Poletai <s0558796@htw-berlin.de>
2019-01-22 10:06:33 +0100

    show badges

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {Storage} from "@ionic/storage";
 
 @IonicPage()
@@ -9,6 +9,8 @@ import {Storage} from "@ionic/storage";
 })
 export class SettingsPage {
   history: number[];
+  totalCardsPlayed: number;
+  cardHistory: number[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
   }
@@ -19,7 +21,18 @@ export class SettingsPage {
 
   public async showHistory(){
     this.history= await this.storage.get('history');
+    console.log(this.history);
   }
+
+  public async showBadges(){
+    this.storage.get('history').then((historie) => {
+      this.cardHistory = JSON.parse(historie) || [];
+      this.totalCardsPlayed = this.cardHistory.length;
+      console.log(this.totalCardsPlayed);
+    })
+
+  }
+
 
   public clearHistory(){
     this.storage.clear().then(() => {console.log('Historie entfernt')});
